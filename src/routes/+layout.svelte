f9ca9986ca4f2c1e72e3a439d85357252a1fdc60
Stan Skrivanek <s.skrivanek@icloud.com>
2025-04-24 19:46:51 +0100

    update

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	//  with rune `$props()` we have access ot { children, data, form }
+	let { children } = $props();
+	import '../app.css';
+</script>
+
+<!-- render layout -->
+{@render children()}
